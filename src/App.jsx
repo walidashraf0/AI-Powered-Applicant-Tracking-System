fafccd7b4424cca343f29a5cbf7b071d2e16accc
Walid Ashraf <walidashraf050@gmail.com>
2025-08-20 10:15:20 +0300

    Initialize Project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold">
+        Hello in Build an Enterprise Ready AI Powered Applicant Tracking System
+      </h1>
+    </>
+  );
+}
+
+export default App;
